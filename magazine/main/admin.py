1fc0c14a4a1bb6d43a3652fce3bf8d54d5f3bdbb
SSazontsev <chebb777@gmail.com>
2025-03-27 03:10:00 +0700

    Страница товара + карточки товара + выбор размера

--- magazine/main/admin.py
+++ magazine/main/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Size, Category, ClothingItem, ClothingItemSize
+from .models import Size, Category, ClothingItem, ClothingItemSize, ItemImage
 
 # Register your models here.
 
 class ClothingItemSizeInline(admin.TabularInline):
     model = ClothingItemSize
+    extra = 4
+
+class ItemImageInline(admin.TabularInline):
+    model = ItemImage
     extra = 4
 
 @admin.register(Size)
@@ -26,4 +30,4 @@ class ClothingItemAdmin(admin.ModelAdmin):
     list_filter = ('available', 'category')
     prepopulated_fields = {'slug': ('name', )}
     ordering = ('-created_at', )
-    inlines = [ClothingItemSizeInline]
+    inlines = [ClothingItemSizeInline, ItemImageInline]
